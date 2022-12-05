034301617831d22c937be621b3f90e64c97fd838
abdellahelaajjouri <abdellah.elaajjouri@gmail.com>
2022-12-05 19:49:30 +0100

    'home-finished'

--- lnadingpage/src/containers/home/Home.jsx
+++ lnadingpage/src/containers/home/Home.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Hero from "../../assets/home.svg";
 import "./Home.css";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div name="about" className="Lasles__Home section__padding">
+      <div className="Lasles__Home-content">
+        <div className="Lasles__Home-content__title">
+          Want anything to be easy with <span> LaslesVPN.</span>
+        </div>
+        <div className="Lasles__Home-content__text">
+          Provide a network for all your needs with ease and fun using LaslesVPN
+          discover interesting features from us.
+        </div>
+
+        <div className="Lasles__Home-content__button">
+          <button> Get Started</button>
+        </div>
+      </div>
+      <div className="Lasles__Home-hero">
+        <img src={Hero} alt="Lasles Home hero" />
+      </div>
+    </div>
+  );
 }
