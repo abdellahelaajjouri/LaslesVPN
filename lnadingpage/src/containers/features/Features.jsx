95602e9300f897985aca2f2b2f09ed4cc0fe6847
abdellahelaajjouri <abdellah.elaajjouri@gmail.com>
2022-12-05 22:02:29 +0100

    'Responsive__features'

--- lnadingpage/src/containers/features/Features.jsx
+++ lnadingpage/src/containers/features/Features.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 import "./Features.css";
+import features from "../../assets/features.svg";
+import check from "../../assets/featuresCheck.png";
 export default function Features() {
-  return <div>Features</div>;
+  return (
+    <div name="features" className="Lasles__features section__padding">
+      <div className="Lasles__features-hero">
+        <img src={features} alt="features" />
+      </div>
+      <div className="Lasles__features-content ">
+        <h1 className="Lasles__features-content__title">
+          We Provide Many Features You Can Use
+        </h1>
+        <p className="Lasles__features-content__text">
+          You can explore the features that we provide with fun and have their
+          own functions each feature.
+        </p>
+        <ul className="Lasles__features-content__list">
+          <li>
+            {" "}
+            <img src={check} alt="check" /> Powerfull online protection.
+          </li>
+          <li>
+            {" "}
+            <img src={check} alt="check" /> Internet without borders.
+          </li>
+          <li>
+            {" "}
+            <img src={check} alt="check" /> Supercharged VPN
+          </li>
+          <li>
+            {" "}
+            <img src={check} alt="check" /> No specific time limits.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- lnadingpage/src/containers/infos/Infos.jsx
+++ lnadingpage/src/containers/infos/Infos.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 import "./Infos.css";
+import infoLocation from "../../assets/infoLocation.png";
+import infoServer from "../../assets/infoServer.png";
+import infoUser  from "../../assets/infoUser.png";
 export default function Infos() {
-  return <div>Infos</div>;
+  return (
+    <div className="Lasles__Infos section__margin">
+      <div className="Lasles__Infos-container">
+        <div className="Lasles__Infos-container__info">
+          <div className="Lasles__Infos-container__info-icon">
+            <img src={infoUser} alt="info User" />
+          </div>
+          <div className="Lasles__Infos-container__info-content">
+            <p className="Lasles__Infos-container__info-content__title">90+</p>
+            <p className="Lasles__Infos-container__info-content__text">Users</p>
+          </div>
+          
+        </div>
+        <hr />
+        <div className="Lasles__Infos-container__info">
+          <div className="Lasles__Infos-container__info-icon">
+            <img src={infoLocation} alt="info Location" />
+          </div>
+          <div className="Lasles__Infos-container__info-content">
+            <p className="Lasles__Infos-container__info-content__title">30+</p>
+            <p className="Lasles__Infos-container__info-content__text">
+              Locations
+            </p>
+          </div>
+        </div>
+        <hr />
+
+        <div className="Lasles__Infos-container__info">
+          <div className="Lasles__Infos-container__info-icon">
+            <img src={infoServer} alt="info Server" />
+          </div>
+          <div className="Lasles__Infos-container__info-content">
+            <p className="Lasles__Infos-container__info-content__title">50+</p>
+            <p className="Lasles__Infos-container__info-content__text">
+              Servers
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
