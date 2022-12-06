50b2728b20c9257f15af43b8496eb7bf45f4465e
abdellahelaajjouri <abdellah.elaajjouri@gmail.com>
2022-12-06 01:27:32 +0100

    'Responsive__prices'

--- lnadingpage/src/components/plan/Plan.jsx
+++ lnadingpage/src/components/plan/Plan.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import "./Plan.css";
-function Plan() {
-  return <div>Plan</div>;
+import hero from "../../assets/Plan.svg";
+import { BsCheckLg } from "react-icons/bs";
+function Plan({ list, title, Price }) {
+  return (
+    <div className="Lasles__plans-container__plan">
+      <div className="Lasles__plans-container__plan-hero">
+        <img src={hero} alt="Plan hero" />
+      </div>
+      <div className="Lasles__plans-container__plan-content">
+        <h3 className="Lasles__plans-container__plan-content__title">
+          {title}
+        </h3>
+        <div className="Lasles__plans-container__plan-content__text">
+          {list.map((item) => {
+            return (
+              <p>
+                <BsCheckLg color={"#2FAB73"} size={13} /> &nbsp; &nbsp; &nbsp;{" "}
+                {item}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="Lasles__plans-container__plan-footer">
+        <h2 className="Lasles__plans-container__plan-footer__Price">{Price}</h2>
+        <div className="Lasles__plans-container__plan-footer__button">
+          <button>Select</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Plan;
--- lnadingpage/src/containers/plans/Plans.jsx
+++ lnadingpage/src/containers/plans/Plans.jsx
@@ -3,12 +3,42 @@ import "./Plans.css";
 import { Plan } from "../../components/index.js";
 
 function Plans() {
+  const freePlan = [
+    "Unlimited Bandwitch",
+    "Encrypted Connection",
+    "No Traffic Logs",
+    "Works on All Devices",
+  ];
+  const standardPlan = [
+    "Unlimited Bandwitch",
+    "Encrypted Connection",
+    "Yes Traffic Logs",
+    "Works on All Devices",
+    "connect Anyware",
+  ];
+  const premiumPlan = [
+    "Unlimited Bandwitch",
+    "Encrypted Connection",
+    "Yes Traffic Logs",
+    "Works on All Devices",
+    "connect Anyware",
+    "Get New Features",
+  ];
   return (
-    <div>
-      Plans
-      <Plan />
-      <Plan />
-      <Plan />
+    <div name="pricing" className="Lasles__plans section__margin">
+      <header className="Lasles__plans-header">
+        <h1 className="Lasles__plans-header__title">Choose Your Plan</h1>
+        <p className="Lasles__plans-header__text">
+          Let's choose the package that is best for you and explore it happily
+          and cheerfully.
+        </p>
+      </header>
+
+      <main className="Lasles__plans-container">
+        <Plan list={freePlan} title="Free Plan" Price="Free" />
+        <Plan list={standardPlan} title="Standard Plan" Price="$9/mo" />
+        <Plan list={premiumPlan} title="premium Plan" Price="$12/mo" />
+      </main>
     </div>
   );
 }
